8206192642065e24b27cb210c133b1b8a331e844
Dmitry O. Markov <markov.dm@inbox.ru>
2020-05-21 00:05:52 +0300

    Add comments

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,4 +1,9 @@
+# A program that selects an arbitrary card of the playing deck
+
+# Card values array
 values = %w[2 3 4 5 6 7 8 9 10 J Q K A]
+
+# Suits array in Unicode
 suits = ["\u2660", "\u2665", "\u2666", "\u2663"]
 
 puts "#{values.sample}#{suits.sample.encode('UTF-8')}"
